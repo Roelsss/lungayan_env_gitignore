1556716cdf1888da9a53d6ce818be1b212097989
Roelsss <roellungayan@gmail.com>
2024-11-18 20:16:33 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+# Load greeting message from environment variable
+Message = os.getenv("Message", "Hello, Roels")
+
+
+@app.route('/')
+def home():
+    return jsonify({"message": Message})
+
+if __name__ == '__main__':
+    app.run(debug=True)
